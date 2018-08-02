6d1dc9eb6e2ea5b1d00b64eb2c245691fb9d7446
hanhui90 <975831981@qq.com>
2018-08-02 15:39:53 +0800

    修改hello类的实现内容

--- app/src/main/java/com/example/hh/uibestpractice/Hello.java
+++ app/src/main/java/com/example/hh/uibestpractice/Hello.java
@@ -2,6 +2,26 @@ package com.example.hh.uibestpractice;
 
 public class Hello {
     /*
-    * 测试Hello类，填充内容
+    * 测试Hello类，实现类的内容，面向对象开发
     * */
+
+    private String helloWorld;
+
+    private String helloCode;
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+    public String getHelloCode() {
+        return helloCode;
+    }
+
+    public void setHelloCode(String helloCode) {
+        this.helloCode = helloCode;
+    }
 }
